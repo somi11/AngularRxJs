9f09db516c2059abcf053dc616a4263f742850fe
somi11 <somisun41@gmail.com>
2025-01-20 09:22:35 +0500

    setup product service

--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 
 import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
     selector: 'pm-product-list',
@@ -10,11 +12,24 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
     standalone: true,
   imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit , OnDestroy {
+  sub! : Subscription;
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts()
+    .pipe(
+      tap(() => console.log("In product list comp pipeline"))
+    )
+    .subscribe(
+     products => this.products = products
+    )
+  }
+  ngOnDestroy(): void {
+   this.sub.unsubscribe();
+  }
   // Just enough here for the template to compile
   pageTitle = 'Products';
   errorMessage = '';
-
+private productService = inject(ProductService)
   // Products
   products: Product[] = [];
 
